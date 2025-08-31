f0c87cd72a38a3b386502c702df39d81ac825dd0
gabrielzepeda1 <gabriel.zepeda@solidlogix.com>
2025-08-31 18:46:50 -0400

    updated wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -18,8 +18,8 @@ config.colors = {
 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
 
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 19
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 16
 
 config.window_decorations ="RESIZE"
 
@@ -28,6 +28,6 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
 
-config.enable_tab_bar = false
+-- config.enable_tab_bar = false
 
 return config

5235f898acc7c2d287bc17dbf488b203c86078e6
gabrielzepeda1 <gabriel.zepeda@solidlogix.com>
2025-09-14 21:47:13 -0400

    update wezterm theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,22 +6,24 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
-config.colors = {
-	foreground = "#CBE0F0",
-	background = "#011423",
-	cursor_bg = "#47FF9C",
-	cursor_border = "#47FF9C",
-	cursor_fg = "#011423",
-	selection_bg = "#033259",
-	selection_fg = "#CBE0F0",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
-}
+-- config.colors = {
+-- 	foreground = "#CBE0F0",
+-- 	background = "#011423",
+-- 	cursor_bg = "#47FF9C",
+-- 	cursor_border = "#47FF9C",
+-- 	cursor_fg = "#011423",
+-- 	selection_bg = "#033259",
+-- 	selection_fg = "#CBE0F0",
+-- 	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+-- 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+-- }
+
+-- config.color_scheme = 'Galaxy'
+config.color_scheme = 'GruvboxDarkHard'
 
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 16
 
-config.window_decorations ="RESIZE"
 
 
 config.window_decorations = "RESIZE"

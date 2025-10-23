4d74bf6a4b3b7d47737fb4a21d1a0f0fe4ca5521
gabrielzepeda1 <gabrielzepeda846@gmail.com>
2025-10-23 18:24:27 -0400

    updated wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,4 +1,7 @@
 -- Pull in the wezterm API
+--
+--
+--
 local wezterm = require("wezterm")
 
 -- This will hold the configuration.
@@ -6,30 +9,24 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
--- config.colors = {
--- 	foreground = "#CBE0F0",
--- 	background = "#011423",
--- 	cursor_bg = "#47FF9C",
--- 	cursor_border = "#47FF9C",
--- 	cursor_fg = "#011423",
--- 	selection_bg = "#033259",
--- 	selection_fg = "#CBE0F0",
--- 	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
--- 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
--- }
+config = {
+	automatically_reload_config = true,
+	enable_tab_bar = false, 
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE", -- disable the title bar but enable the resizable border
+	window_background_opacity = 0.9,
+	macos_window_background_blur = 60,
+	font_size = 14,
+	font = wezterm.font("JetBrains Mono", {weight="Bold"}),
+	color_scheme = "nord",
+	default_cursor_style = "BlinkingBar",
+	window_padding={ 
+		left = 5, right = 5, top = 5, bottom = 5
 
--- config.color_scheme = 'Galaxy'
-config.color_scheme = 'GruvboxDarkHard'
-
-config.font = wezterm.font("JetBrains Mono")
-config.font_size = 16
-
-
-
-config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.9
-config.macos_window_background_blur = 60
-
--- config.enable_tab_bar = false
+	},
+	keys = {
+		{key="Enter", mods="SHIFT", action=wezterm.action{SendString="\x1b\r"}}
+	}
+} 
 
 return config
